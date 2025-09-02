70fff9f0e9f934e0900ba063c4cbad0749caaf8e
pablobuenaposada <pablo.buenaposada@gmail.com>
2025-09-02 23:40:45 +0200

    add secret_key env var

--- src/api/main/settings.py
+++ src/api/main/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-b*y(4l-3paxd01jk03i2m&+g0!uv4wumzg20(0(_-sw4ro7a@0"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
