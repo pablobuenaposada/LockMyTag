1a247bcd622a8c979e08134e70d5676474e1cbd8
pablobuenaposada <pablo.buenaposada@gmail.com>
2025-09-22 23:16:21 +0200

    colored status

--- src/backoffice/locks/admin.py
+++ src/backoffice/locks/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Lock
 
@@ -7,7 +8,7 @@ class LockAdmin(admin.ModelAdmin):
     list_display = (
         "id",
         "tag__name",
-        "status",
+        "colored_status",
         "radius",
         "schedule_day",
         "schedule_start_time",
@@ -15,6 +16,13 @@ class LockAdmin(admin.ModelAdmin):
     )
     readonly_fields = ("created", "modified")
     list_filter = ["tag__name"]
+
+    def colored_status(self, obj):
+        color = "green" if obj.status == "active" else "red"
+        return format_html('<span style="color: {};">{}</span>', color, obj.status)
+
+    colored_status.admin_order_field = "status"
+    colored_status.short_description = "Status"
 
 
 admin.site.register(Lock, LockAdmin)
